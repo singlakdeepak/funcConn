c6fc0755860ab08a2ddccb1345d8370270ab3dd6
singlakdeepak <singlakdeepak@github.com>
2017-09-07 15:15:04 +0530

    Added comments in Featdirs

--- funcConnGUI/editfeat.cpp
+++ funcConnGUI/editfeat.cpp
@@ -1,3 +1,8 @@
+/* Author: Deepak Singla (singlakdeepak5@gmail.com)
+ * Window for selecting FeatDirectories.
+ *
+ */
+
 #include "editfeat.h"
 #include "ui_editfeat.h"
 #include <QFile>
@@ -39,7 +44,7 @@ void editFeat::on_pushButton_Save_clicked()
      * On clicking the save button in the window, it checks
      * whether the folders written row by row in the text editor
      * are present or not. If all files are present, it saves them
-     * in a file with the name Group_<IndexNo>_<FeatDirs>.txt
+     * in a file with the name Group_<IndexNo>_FeatDirs.txt
      *
      */
     int Groupindex = ui->comboBox_Groups->currentIndex();
@@ -60,9 +65,19 @@ void editFeat::on_pushButton_Save_clicked()
 
 void editFeat::on_comboBox_Groups_currentIndexChanged(int index)
 {
+    /*
+     * When the index on the comboBox is changed, this signal gets activated.
+     * It opens the file with name Group_<IndexNo>_FeatDirs.txt in the
+     * built application directory if it is present and flushes the contents
+     * into the PlainTextEditor.
+     *
+     */
     ui->TextEdit_FeatDirs->clear();
 
     QFile file_Feat( WorkingDir+ "/Group_"+QString::number(index+1)+"_FeatDirs.txt");
+
+    // If the following file is present, then only the file is read.
+    //Then it flushes the content into Text Editor.
     if (file_Feat.open(QFile::ReadOnly | QFile::Text)){
     QTextStream in_Feat(&file_Feat);
     QString text_Feat = in_Feat.readAll();
@@ -74,10 +89,23 @@ void editFeat::on_comboBox_Groups_currentIndexChanged(int index)
 
 void editFeat::on_ld_featDirList_clicked()
 {
+    /*
+     * This signal is for opening a Feat Directories list.
+     * The list should be in *.txt format and should contain the
+     * absolute paths of the Feat Directories in each row.
+     *
+     * It checks the String currentFeatDir which stores the
+     * initial location from where Feat Directory had been picked.
+     * If the current path still exists, then it opens the
+     * Directory Chooser from that point else it opens the
+     * Directory Chooser from the common path assigned in the scripts.
+     */
+
     QFile Fout(currentFeatDir) ;
     QString new_input;
     if (Fout.exists())
     {
+
         new_input=QFileDialog::getOpenFileName(this,
                                                tr("Open Feat File"),
                                                     currentFeatDir,
@@ -93,6 +121,9 @@ void editFeat::on_ld_featDirList_clicked()
                                                 "Text File (*.txt)"
                                                    );
     }
+    // If 'Cancel' is clicked, then the string will be null,
+    // hence it will change the Text Editor data only if some file
+    // is chosen.
     if (new_input!=NULL){
         QFile file(new_input);
         if(!file.open(QIODevice::ReadOnly)) {
